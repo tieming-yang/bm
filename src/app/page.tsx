9e029053f2ef2b532d1b21bf8d454a020f79a4ff
Snow Yáng <ytm199891@gmail.com>
2025-03-18 14:18:02 +0800

    fixup!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { motion } from "framer-motion"
 import { ImageGallery } from "../components/image-gallery"
 import { artworks } from "../data/artworks"
+import { Suspense } from "react"
 
 export default function Home() {
   return (
@@ -21,7 +22,9 @@ export default function Home() {
         </p>
       </motion.div>
 
-      <ImageGallery artworks={artworks} initialLimit={4} />
+      <Suspense>
+        <ImageGallery artworks={artworks} initialLimit={4} />
+      </Suspense>
     </div>
   )
 }
