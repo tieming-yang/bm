5aacf466c463f0cd12cc7924da911b799a3a4d16
Snow Yáng <ytm199891@gmail.com>
2025-03-18 19:52:36 +0800

    Change to zh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="zh" suppressHydrationWarning>
       <head>{/* <link rel="icon" href="/favicon.ico" sizes="any" /> */}</head>
       <body className={inter.className}>
         <ThemeProvider
