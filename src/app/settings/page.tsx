2c8c0fb8a62a5c870e830fc1ec66cc09aa34c5e2
Snow Yáng <ytm199891@gmail.com>
2025-05-25 15:58:27 +0800

    Refactor SettingsPage to use Card component and integrate Avatar for user display

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -8,6 +8,20 @@ import Loading from "@/components/loading";
 import Image from "next/image";
 import useTranslation from "@/hooks/useTranslation";
 import SignOutButton from "../signout/signout-button";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 type Props = {};
 
@@ -29,29 +43,43 @@ export default function SettingsPage({}: Props) {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-dvh">
-      <h1 className="text-3xl font-bold mb-4">{t("settings.title")}</h1>
-      <div className="w-full max-w-md p-4 space-y-5 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold mb-2">{t("donator.title")}</h2>
-        <Image src={donator?.photoURL || ""} alt={donator?.name || ""} width={100} height={100} />
-        <p>
-          <strong>{t("donator.email")}:</strong> {donator?.email}
-        </p>
-        <p>
-          <strong>{t("donator.name")}:</strong> {donator?.name || ""}
-        </p>
-        <p>
-          <strong>{t("donator.total")}:</strong> {donator?.totalDonated}
-        </p>
-        <p>
-          <strong>{t("donator.preferredLanguage")}:</strong> {donator?.preferredLanguage || ""}
-        </p>
-        <p>
-          <strong>{t("donator.newsletter")}:</strong>{" "}
-          {donator?.newsletterOptIn ? commonT("yes") : commonT("no")}
-        </p>
-      </div>
+      <Card className="md:w-[750px] w-[90%]">
+        <CardHeader>
+          <CardTitle>{t("settings.title")}</CardTitle>
+        </CardHeader>
+        <CardContent className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <section className="place-self-center">
+            <Avatar className="w-24 h-24 md:w-48 md:h-48">
+              <AvatarImage src={donator?.photoURL!} alt={donator?.name || ""} />
+              <AvatarFallback>{donator?.name}</AvatarFallback>
+            </Avatar>
+          </section>
 
-      <SignOutButton />
+          <section className="space-y-4">
+            <h2 className="text-xl font-semibold mb-2">{t("donator.title")}</h2>
+
+            <p>
+              <strong>{t("donator.email")}:</strong> {donator?.email}
+            </p>
+            <p>
+              <strong>{t("donator.name")}:</strong> {donator?.name || ""}
+            </p>
+            <p>
+              <strong>{t("donator.total")}:</strong> {donator?.totalDonated}
+            </p>
+            <p>
+              <strong>{t("donator.preferredLanguage")}:</strong> {donator?.preferredLanguage || ""}
+            </p>
+            <p>
+              <strong>{t("donator.newsletter")}:</strong>{" "}
+              {donator?.newsletterOptIn ? commonT("yes") : commonT("no")}
+            </p>
+          </section>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <SignOutButton />
+        </CardFooter>
+      </Card>
     </div>
   );
 }
