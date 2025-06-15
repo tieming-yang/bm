ca8a0dad2d3b0dc601f9c9855bd946632f8b4db4
Snow Yáng <ytm199891@gmail.com>
2025-06-14 20:13:56 -0500

    Remove console log statement from image load handler in ImageGallery component

--- src/components/image-gallery.tsx
+++ src/components/image-gallery.tsx
@@ -286,7 +286,6 @@ export function ImageGallery({
                       sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
                       priority
                       onLoad={() => {
-                        console.log("Image loaded");
                         setIsImageLoading(false);
                       }}
                     />
