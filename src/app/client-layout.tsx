d496d5d2c1d96c81a57252fff8c04620d0d53599
Snow Yáng <ytm199891@gmail.com>
2025-06-16 09:10:30 -0500

    Refactor ClientRoot component: restore AuroraHero wrapping around children

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,14 +1,15 @@
 "use client";
+
 import { AuroraHero } from "@/components/aurora-hero";
 import "../lib/i18n";
 
 function ClientRoot({ children }: React.PropsWithChildren) {
   return (
     <div>
-      {/*  <AuroraHero> */}
-      {children}
-      {/* <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-right" /> */}
-      {/* </AuroraHero> */}
+      <AuroraHero>
+        {children}
+        {/* <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-right" /> */}
+      </AuroraHero>
     </div>
   );
 }
