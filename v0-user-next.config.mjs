94ae91c2c3468e48abbea264ea00c735b406e7e1
Snow Yáng <ytm199891@gmail.com>
2025-11-06 19:16:04 -0600

    chore: remove v0 config

--- v0-user-next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['localhost', 'images.unsplash.com', 'picsum.photos'],
-  },
-}
-
-export default nextConfig
-
