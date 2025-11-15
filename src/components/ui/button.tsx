0ac653eedfcd3c332a219c31d723c0242243dcec
Snow Yáng <ytm199891@gmail.com>
2025-11-14 22:28:46 -0600

    refactor: update default size padding in Button component for consistency

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "h-10 p-2",
         sm: "h-9 rounded-3xl px-3",
         lg: "h-11 rounded-3xl px-8",
         icon: "h-10 w-10",
