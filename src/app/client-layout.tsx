992f3b4b240b1d4b698c3cffa19e70efc49a8af9
Snow Yáng <ytm199891@gmail.com>
2025-11-18 16:06:43 -0600

    style: change root transition duration

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -8,13 +8,13 @@ function ClientRoot({ children }: React.PropsWithChildren) {
   const pathname = usePathname();
 
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.4 }}>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
       <motion.div
         key={pathname}
         className="fixed inset-0 z-50 pointer-events-none"
         initial={{ opacity: 1, backdropFilter: "blur(20px)" }}
         animate={{ opacity: 0, backdropFilter: "blur(0px)" }}
-        transition={{ duration: 1, ease: "easeOut" }}
+        transition={{ duration: 1.5, ease: "easeOut" }}
       />
       {children}
     </motion.div>
