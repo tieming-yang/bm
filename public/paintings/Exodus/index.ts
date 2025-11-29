0d685f60e86b4734a3865d956bcba519434cd0dd
Snow <61799702+tieming-yang@users.noreply.github.com>
2025-11-29 21:43:57 +0000

    fix: Windows-incompatible filenames

--- public/paintings/Exodus/index.ts
+++ public/paintings/Exodus/index.ts
@@ -1,2 +1,2 @@
-export { default as Exodus_1_1v22 } from "./Exodus1:1-22.webp";
-export { default as Exodus_18v1_10v1 } from "./Exodus18:1-10:1.webp";
+export { default as Exodus_1_1v22 } from "./Exodus1-1-22.webp";
+export { default as Exodus_18v1_10v1 } from "./Exodus18-1-10:1.webp";
