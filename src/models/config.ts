9a17b5dfb4287da1beb05fd93df9216c112edf8d
Snow Yáng <ytm199891@gmail.com>
2025-12-03 14:38:30 -0600

    feat: add og url

--- src/models/config.ts
+++ src/models/config.ts
@@ -1,11 +1,12 @@
 const isProd = process.env.NODE_ENV === "production";
 const aspectRatio = 1.74 / 1;
 const baseUrl = isProd ? "https://beyond-media.art/" : "http://localhost:3000";
-
+const OGImage = "https://beyond-media.art/opengraph-image.png"
 const Config = {
   isProd,
   aspectRatio,
   baseUrl,
+  OGImage
 };
 
 export default Config;
