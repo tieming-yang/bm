7178682bc64f9e2dcb2b93569f5ba1d81b7ba995
Snow Yáng <ytm199891@gmail.com>
2025-12-02 20:33:20 -0600

    fix: beyond-music title

--- src/app/(works)/beyond-music/client-page.tsx
+++ src/app/(works)/beyond-music/client-page.tsx
@@ -34,16 +34,19 @@ export default function ClientBeyondMusicPage() {
   }
 
   return (
-    <div className="container px-4 py-32 mx-auto space-y-16 min-h-svh">
+    <div className="container px-4 pt-16 mx-auto space-y-16 min-h-svh">
+      <h1 className="text-4xl font-bold leading-tight tracking-tight text-balance md:text-5xl">
+        {t("beyondMusic.hero.title")}
+      </h1>
       {songs && (
-        <ul>
+        <ul className="flex flex-col gap-y-5">
           {songs.map((song) => {
             const { id, title, fileUrl } = song;
 
             return (
               <li
                 key={id}
-                className="relative overflow-hidden border shadow-lg rounded-full border-primary/20 bg-linear-to-br from-primary/10 via-background to-secondary/10 py-3 shadow-primary/10"
+                className="bg-primary-gradient-50 relative overflow-hidden border shadow-lg rounded-full py-3 shadow-primary/10"
               >
                 <Link href={`/beyond-music/${encodeURIComponent(title)}`}>
                   <p className="text-xl text-center">{title}</p>
