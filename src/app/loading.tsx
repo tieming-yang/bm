9c8766e0527512fb305763fe5d1ed3801a433fb5
Snow Yáng <ytm199891@gmail.com>
2025-12-04 10:34:01 -0600

    fix: inline loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -15,7 +15,7 @@ export default function Loading({ isInlined = false, show = true }: LoadingProps
 
   if (isInlined) {
     return (
-      <div className="w-full h-full bg-black/30 backdrop-blur-md z-50 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center-safe">
+      <div className="w-full h-full bg-black/30 backdrop-blur-md z-50 absolute rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center-safe">
         <Loader2 className="animate-spin text-primary" />
       </div>
     );
