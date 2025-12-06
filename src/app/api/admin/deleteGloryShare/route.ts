9ecc6dfd5345c77946aeef810e74959124237e74
Snow Yáng <ytm199891@gmail.com>
2025-12-06 12:41:47 -0600

    feat: add delete glory share admin api

---
+++ src/app/api/admin/deleteGloryShare/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import firebaseAdmin from "@/lib/firebase/firebase-admin";
+import { FieldValue } from "firebase-admin/firestore";
+
+const ADMIN_KEY = "NAEBO777!!!"
+
+export async function POST(req: NextRequest) {
+  const { adminId, uid } = await req.json();
+  if (adminId !== ADMIN_KEY) return NextResponse.json({ error: "No Uid" }, { status: 401 })
+
+  if (!uid)
+    return NextResponse.json({ error: "No Uid" }, { status: 400 })
+
+  try {
+    await firebaseAdmin.db.doc(`profiles/${uid}`).update({
+      joinedGloryShare: false,
+      gloryShare: FieldValue.delete(),
+      customerDetails: FieldValue.delete(),
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: error }, { status: 500 })
+  }
+
+  return NextResponse.json({ success: true });
+}
