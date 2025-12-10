1286b3292f97ff94ffbe04c387ad994ebfacdcf4
Snow Yáng <ytm199891@gmail.com>
2025-12-10 16:16:39 -0600

    wip! subscription webhook integration

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -20,6 +20,8 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: `Invalid signature: ${error}` }, { status: 400 });
   }
 
+  console.log("📧 Incoming Event:", event.type, "-----")
+
   try {
     switch (event.type) {
       case "checkout.session.completed": {
@@ -29,12 +31,12 @@ export async function POST(req: NextRequest) {
         // console.log("========================session=====================")
         const uid = session.metadata?.uid;
         if (!uid) {
-          throw new Error("Missing Uid in checkout session")
+          throw new Error(`Missing Uid in ${event.type}`)
         }
 
         const memberDetails = {
-          name: session.customer_details?.name,
-          phone: session.customer_details?.phone,
+          name: session.customer_details?.name ?? null,
+          phone: session.customer_details?.phone ?? null,
           address: {
             city: session.customer_details?.address?.city ?? null,
             country: session.customer_details?.address?.country ?? null,
@@ -44,15 +46,14 @@ export async function POST(req: NextRequest) {
             state: session.customer_details?.address?.state ?? null,
           }
         }
-        const commonData = {
+        const transactionData = {
           sessionId: session.id,
           paymentIntentId: session.payment_intent,
           customerId: session.customer,
           paymentStatus: session.payment_status,
           metadata: session.metadata ?? {},
-          createdAt: FieldValue.serverTimestamp(),
           amountTotal: session.amount_total,
-          currency: session.currency
+          currency: session.currency,
         }
 
         const profileRef = firebaseAdmin.db.doc(`profiles/${uid}`);
@@ -63,14 +64,16 @@ export async function POST(req: NextRequest) {
             profileRef.set(
               {
                 memberType: MemberType.LiftTime,
-                joinedAt: FieldValue.serverTimestamp(),
                 lastTransactionId: transactionRef.id,
                 memberDetails,
+                updatedAt: FieldValue.serverTimestamp()
               },
               { merge: true }
             ),
             transactionRef.set({
-              ...commonData,
+              ...transactionData,
+              updatedAt: FieldValue.serverTimestamp(),
+              createdAt: FieldValue.serverTimestamp(),
             }),
           ]);
         } else if (session.mode === "subscription") {
@@ -85,80 +88,84 @@ export async function POST(req: NextRequest) {
             }
           });
 
+          const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+          const subscriptionItem = subscription.items.data[0]
+
           const subscriptionRef = profileRef.collection("subscriptions").doc(subscriptionId)
 
           await Promise.all([
             profileRef.set(
               {
                 memberType: session.amount_total === 770 ? MemberType.Monthly : MemberType.Yearly,
-                joinedAt: FieldValue.serverTimestamp(),
                 lastTransactionId: transactionRef.id,
                 lastSubscriptionId: subscriptionId,
                 memberDetails,
+                updatedAt: FieldValue.serverTimestamp()
               },
               { merge: true }
             ),
             transactionRef.set({
-              ...commonData,
+              ...transactionData,
+              updatedAt: FieldValue.serverTimestamp(),
+              createdAt: FieldValue.serverTimestamp(),
             }),
             subscriptionRef.set({
-              subscriptionId,
-            })
+              id: subscriptionId,
+              status: subscription.status,
+              startDate: subscription.start_date,
+              endAt: subscription.ended_at,
+              currentPeriodStart: subscriptionItem.current_period_start,
+              currentPeriodEnd: subscriptionItem.current_period_end,
+              updatedAt: FieldValue.serverTimestamp(),
+              createdAt: FieldValue.serverTimestamp(),
+            }, { merge: true })
           ]);
         }
         break;
       }
 
-
+      /**
+       * Renew
+       */
       case "invoice.paid": {
         const invoice = event.data.object;
-        // Check if this is a renewal (not the first payment)
-        console.log("========================invoice=====================")
-        console.log("invoice", invoice)
-        console.log("========================invoice=====================")
         if (invoice.billing_reason === 'subscription_cycle') {
           if (invoice.lines.data.length > 1) {
             throw new Error("Invoice contains multiple items")
           }
+
           const lineItem = invoice.lines.data[0] as Stripe.InvoiceLineItem;
           const subscriptionId = lineItem.parent?.subscription_item_details?.subscription
           if (!subscriptionId) {
             throw new Error("Missing subscription id in invoice")
           }
-          const subscriptions = await stripe.subscriptions.retrieve(subscriptionId);
-          const subscription = subscriptions.items.data[0]
-          
-          const uid = subscription.metadata.uid
+          const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+          const uid = invoice.parent?.subscription_details?.metadata?.uid ?? subscription.metadata.uid
           if (!uid) {
             throw new Error("Missing uid in subscription")
           }
 
+          const subscriptionItem = subscription.items.data[0]
+
           const profileRef = firebaseAdmin.db.doc(`profiles/${uid}`);
           const subscriptionRef = profileRef.collection("subscriptions").doc(subscriptionId)
 
-          await Promise.all([
-            profileRef.set(
-              {
-                memberEndAt: lineItem.period.end
-              },
-              { merge: true }
-            ),
-            subscriptionRef.update({
-              status: subscriptions.status,
-              startDate: subscriptions.start_date,
-              endAt: subscriptions.ended_at,
-              currentPeriodStart: subscription.current_period_start,
-              currentPeriodEnd: subscription.current_period_end,
-            })
-          ]);
-
+          await subscriptionRef.set({
+            status: subscription.status,
+            startDate: subscription.start_date,
+            endAt: subscription.ended_at,
+            currentPeriodStart: subscriptionItem.current_period_start,
+            currentPeriodEnd: subscriptionItem.current_period_end,
+            updatedAt: FieldValue.serverTimestamp()
+          }, { merge: true })
         }
         break;
       }
 
       case 'customer.subscription.deleted': {
         const cancelledSubscription = event.data.object;
-        const { id: subscriptionId, metadata, status, start_date, ended_at, cancel_at } = cancelledSubscription
+        const { id: subscriptionId, metadata, status, start_date, ended_at, canceled_at } = cancelledSubscription
         const uid = metadata.uid
         if (!uid) {
           throw new Error("Missing Uid in subscription deleted")
@@ -171,24 +178,25 @@ export async function POST(req: NextRequest) {
           profileRef.set(
             {
               memberType: MemberType.Free,
+              updatedAt: FieldValue.serverTimestamp(),
             },
             { merge: true }
           ),
-          subscriptionRef.update({
+          subscriptionRef.set({
             status: status,
-            cancelAt: cancel_at,
+            canceledAt: canceled_at,
             startDate: start_date,
-            endAt: ended_at
-          })
+            endAt: ended_at,
+            updatedAt: FieldValue.serverTimestamp()
+          }, { merge: true })
         ]);
         break;
       }
 
       default: {
-        console.log("========================event type=====================")
-        console.log("event type", event.type)
-        console.log("========================event type=====================")
-        throw new Error("Unsupport Event Type")
+        // console.log(" === event type ===")
+        // console.log("| ", event.type, " |")
+        // console.log(" === event type ===")
       }
 
     }
@@ -198,5 +206,5 @@ export async function POST(req: NextRequest) {
   }
 
 
-  return NextResponse.json({ received: true });
+  return NextResponse.json({ received: true }, { status: 200 });
 }
