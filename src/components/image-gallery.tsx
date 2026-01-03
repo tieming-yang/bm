352df34e81c5a5fe14824ae1b26ca8cb28ab2af5
Norman Yáng <ytm199891@gmail.com>
2026-01-03 15:53:11 -0600

    style: increase text size

--- src/components/image-gallery.tsx
+++ src/components/image-gallery.tsx
@@ -397,7 +397,7 @@ export function ImageGallery({
                           }`}</span>
                         </span>
                       </section>
-                      <p className="text-sm md:text-lg whitespace-pre-wrap leading-8 text-foreground overflow-auto border rounded-md px-3 py-4 bg-accent-foreground/10 md:max-h-[70vh]">
+                      <p className="text-xl md:text-2xl whitespace-pre-wrap leading-8 text-foreground overflow-auto border rounded-md px-3 py-4 bg-accent-foreground/10 md:max-h-[70vh]">
                         {selectedArtwork.scripture}
                       </p>
                     </div>
