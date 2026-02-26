bb9698a9a21c26d1da749a2aef0fee546c22eeed
Norman Yáng <ytm199891@gmail.com>
2026-02-26 15:17:45 -0600

    refactor: clean up bible-artworks/route.ts

--- src/app/api/bible-artworks/route.ts
+++ src/app/api/bible-artworks/route.ts
@@ -1,58 +1,55 @@
-// @ts-nocheck
-
 import { NextResponse } from "next/server";
 import { assertIsDefined } from "@/lib/utils";
-import { Client, PageObjectResponse } from "@notionhq/client";
-import { BibleArtwork, Scripture } from "../../../types/bible-artwork";
-
+import { Client, RichTextItemResponse } from "@notionhq/client";
+import { BibleArtwork } from "../../../types/bible-artwork";
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 const dbID = process.env.BIBLE_ARTWORKS_DB_ID;
 
 export async function GET() {
-  assertIsDefined(notion, "Notion is not defined");
-  assertIsDefined(dbID, "BibleArtworks.dbID is not defined");
-
   try {
+    assertIsDefined(notion, "Notion is not defined");
+    assertIsDefined(dbID, "BibleArtworks.dbID is not defined");
+
     const { results } = await notion.databases.query({
       database_id: dbID,
       sorts: [{ property: "Name", direction: "ascending" }],
       page_size: 100,
     });
 
-    const entries = await Promise.all(
-      results.map(async (page: PageObjectResponse) => {
+    const entries: BibleArtwork[] = await Promise.all(
+      results.map(async (page: any) => {
         const id = page.id;
         const title = page.properties.Name.title[0]?.plain_text ?? "";
         const zh = page.properties.zh?.rich_text ?? "";
         const en = page.properties.en?.rich_text ?? "";
         const book = page.properties.Book.select?.name ?? "";
         const section = page.properties.Section.rich_text[0]?.plain_text ?? "";
-        const artwork = page.properties.Artwork.files[0]?.file.url ?? "";
+        const imageUrl = page.properties.Artwork.files[0]?.file.url ?? null;
         const videoUrl = page.properties.VideoUrl.url ?? null;
         const voUrl = page.properties.VO.files[0]?.file.url ?? null;
         const createdTime = page.created_time;
-        
+
         return {
           id,
           title,
           book,
           section,
           scriptures: {
-            en: en.map((text) => text.plain_text)?.join(""),
-            zh: zh.map((text) => text.plain_text)?.join(""),
+            en: en.map((text: RichTextItemResponse) => text.plain_text)?.join(""),
+            zh: zh.map((text: RichTextItemResponse) => text.plain_text)?.join(""),
           },
-          imageUrl: artwork,
+          imageUrl,
           videoUrl,
           voUrl,
           createdTime,
-        } as BibleArtwork;
+        };
       })
     );
-    const sanitized = entries.filter((entry) => entry.imageUrl !== "");
+    const sanitized = entries.filter((entry) => entry.imageUrl !== null);
 
     return NextResponse.json(sanitized);
   } catch (error) {
     console.error("Error fetching artwork entries:", error);
-    return NextResponse.error("Failed to fetch artwork entries", { status: 500 });
+    return NextResponse.json("Failed to fetch artwork entries", { status: 500 });
   }
 }
