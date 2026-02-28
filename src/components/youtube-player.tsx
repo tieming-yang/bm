ef0cb0bc66382f399afae7409524da3dc3d6afb4
Norman Yáng <ytm199891@gmail.com>
2026-02-27 22:07:06 -0600

    fix: fetch both en and zh artworks

--- src/components/youtube-player.tsx
+++ src/components/youtube-player.tsx
@@ -10,7 +10,7 @@ export function YoutubePlayer({ src, title }: YouTubePlayerProps) {
 
   return (
     <iframe
-      className="aspect-video"
+      className="aspect-video w-px min-w-full"
       src={withLoop}
       title={title}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
