535f1d42237f8fa3d2bc582e33e20f9fe089b5bd
Norman Yáng <ytm199891@gmail.com>
2026-04-16 08:59:47 -0500

    feat: add header exclude

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,10 +2,19 @@
 
 import Logo from "./logo";
 import { Breadcrumb } from "./breadcrumb";
+import { usePathname } from "next/navigation";
 
-type Props = {};
+const EXCLUDE_ROUTE_PREFIXES = ["/sandbox"];
 
-export default function Header({}: Props) {
+export default function Header() {
+  const currentPathname = usePathname();
+
+  const isExcludedRoute = EXCLUDE_ROUTE_PREFIXES.some(
+    (route) => currentPathname === route || currentPathname.startsWith(`${route}/`)
+  );
+
+  if (isExcludedRoute) return null;
+
   return (
     <header className="fixed flex items-start justify-center w-full pt-1 z-100 h-fit">
       <div className="fixed left-3">
