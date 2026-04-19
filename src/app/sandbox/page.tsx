328b240a8fa138bf3f2f61f20c7a4e2dfc204a78
Norman Yáng <ytm199891@gmail.com>
2026-04-19 11:16:52 -0500

    refactor: render from a list

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,14 +1,25 @@
 import Link from "next/link";
+const sandboxes = [
+  {
+    title: "Depth 3D",
+    path: "/sandbox/depth-3d",
+  },
+  {
+    title: "AR",
+    path: "/sandbox/ar",
+  },
+];
 export default function SandBox() {
   return (
     <main className="container min-h-svh flex justify-center items-center-safe">
       <ul className="space-y-5">
-        <li>
-          <Link href="/sandbox/depth-3d">Depth 3D</Link>
-        </li>
-        <li>
-          <Link href="/sandbox/ar">AR</Link>
-        </li>
+        {sandboxes.map((s) => {
+          return (
+            <li key={s.title}>
+              <Link href={s.path}>{s.title}</Link>
+            </li>
+          );
+        })}
       </ul>
     </main>
   );
